57f86e82b2d7e6e01808a9cc55a2a6ebbe73418d
RobertClarke <robert@rjfc.net>
2013-07-10 23:01:31 -0700

    Git build in footer

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,3 +1,3 @@
-<p class="pull-right">Powered by <a href="http://craftstats.com">CraftStats</p>
+<p class="pull-right">Powered by <a href="http://craftstats.com">CraftStats | <?php include 'build.php'; ?></p>
 
 <p>Run by <a href="http://robertclarke.com">Robert Clarke</a> Contact for any assistance.</a></p>

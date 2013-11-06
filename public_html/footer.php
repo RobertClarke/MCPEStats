a4063c7a7b3ff971e32047d67f2a96dd114bc3a7
RobertClarke <robert@rjfc.net>
2013-11-06 01:36:39 -0800

    Update footer.php

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,6 +1,6 @@
 		</div>
 		<footer>
-			<p class="pull-right">Powered by <a href="http://craftstats.com">CraftStats</a></p>
+			<!--<p class="pull-right">Powered by <a href="http://craftstats.com">CraftStats</a></p>-->
 
 	<p>A <a href="http://cubemotion.com">CubeMotion</a> website.</p>
 		</footer>

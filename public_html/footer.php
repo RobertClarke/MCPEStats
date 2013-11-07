b892b8690ab4930355951108231735da7f8a16a0
RobertClarke <robert@rjfc.net>
2013-11-07 00:34:14 -0800

    Footer cleanup

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,18 +1,6 @@
 		</div>
 		<footer>
-		<center><div style="padding-bottom:20px;align:center;">
-		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-		<!-- MCPE Lower Header 2 -->
-		<ins class="adsbygoogle"
-		     style="display:inline-block;width:728px;height:90px"
-		     data-ad-client="ca-pub-8782622759360356"
-		     data-ad-slot="5129381030"></ins>
-		<script>
-		(adsbygoogle = window.adsbygoogle || []).push({});
-		</script>
-		</div></center>
-			<!--<p class="pull-right">Powered by <a href="http://craftstats.com">CraftStats</a></p>-->
-
+	<!--<p class="pull-right">Powered by <a href="http://craftstats.com">CraftStats</a></p>-->
 	<p>A <a href="http://cubemotion.com">CubeMotion</a> website.</p>
 		</footer>
 	</div>

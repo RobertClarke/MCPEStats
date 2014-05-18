6e96bcfcb3b58e58a0adcc858dcedb55c262e5e5
williamtdr <william@cboos.com>
2014-05-18 09:43:30 -0500

    Try II.

--- public_html/index.php
+++ public_html/index.php
@@ -82,6 +82,36 @@ if ($login->messages) {
       </tr>
   </thead>   
   <tbody>
+<tr>
+<?php
+	echo "<td>Lifeboat Survival Games (Hunger Games)</td>";
+	echo "<td>williamtdr</td>";
+	echo '<span class="label label-success">Public</span>';
+	echo "<td>play.lbsg.net</td>";
+	echo "<td>".file_get_contents("http://account.lbsg.net/playercount/tracker.php?server=SG")."/7440</td>";
+	echo '<span class="label label-success">Online</span>';
+?>
+</tr>
+<tr>
+<?php
+	echo "<td>Lifeboat Capture the Flag</td>";
+	echo "<td>williamtdr</td>";
+	echo '<span class="label label-success">Public</span>';
+	echo "<td>ctf.lbsg.net</td>";
+	echo "<td>".file_get_contents("http://account.lbsg.net/playercount/tracker.php?server=CTF")."/720</td>";
+	echo '<span class="label label-success">Online</span>';
+?>
+</tr>
+<tr>
+<?php
+	echo "<td>Lifeboat Infinite Survival</td>";
+	echo "<td>williamtdr</td>";
+	echo '<span class="label label-success">Public</span>';
+	echo "<td>infinity.lbsg.net</td>";
+	echo "<td>".file_get_contents("http://account.lbsg.net/playercount/tracker.php?server=inf")."/480</td>";
+	echo '<span class="label label-success">Online</span>';
+?>
+</tr>
 <?php
 //shuffle($data);
 

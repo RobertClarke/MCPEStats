05e0bf44a9e6f2ba4cc3213f118b8409f5aece6c
RobertClarke <robert@rjfc.net>
2014-06-26 11:59:01 -0700

    Update insert.php

--- public_html/insert.php
+++ public_html/insert.php
@@ -6,6 +6,19 @@ include 'header.php';
     <div id="legend">
       <legend class="">Add a server</legend>
     </div>
+    
+<center>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- MCPEStats Header 2 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-3736311321196703"
+     data-ad-slot="4773859071"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
+
 <div class="eight columns">
 
   <fieldset>

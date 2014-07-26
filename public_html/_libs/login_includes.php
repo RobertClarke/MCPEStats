204efa77e0128720a5594fcf36cccac09c5380b4
Michael Yoo <michael@yoo.id.au>
2014-07-27 01:00:45 +0930

    Fix bug. Staged for testing in dev.

--- public_html/_libs/login_includes.php
+++ public_html/_libs/login_includes.php
@@ -6,7 +6,7 @@
  * Time: 12:20 AM
  */
 
-require_once(__DIR__."../login/libraries/password_compatibility_library.php");
-require_once(__DIR__."../login/config/db.php");
-require_once(__DIR__."../login/config/hashing.php");
-require_once(__DIR__."../login/classes/Login.php");
+require_once(__DIR__."/../login/libraries/password_compatibility_library.php");
+require_once(__DIR__."/../login/config/db.php");
+require_once(__DIR__."/../login/config/hashing.php");
+require_once(__DIR__."/../login/classes/Login.php");

dd60634e125fb74123248dc3fd87d05b1317d391
Michael Yoo <michael@yoo.id.au>
2014-08-09 17:52:07 +0930

    Add Sitemap

---
+++ _cronjobs/build_sitemap.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michael
+ * Date: 9/08/14
+ * Time: 5:40 PM
+ */
+
+require_once(__DIR__."/../public_html/_libs/constants.php");
+
+require_once(__DIR__."/../_libs/Sitemap.php");
+
+$PDO = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+try {
+    $stmt = $PDO->query("SELECT * FROM ServerList1");
+    $Servers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $ex) {
+    exit(1);
+}
+
+$Sitemap = new Sitemap('http://mcpestats.com');
+$Sitemap->setPath("/var/www/mcpestats.com/public_html/");
+
+foreach($Servers as $Server)
+{
+    $Sitemap->addItem("/server/{$Server['id']}", '0.5', 'daily');
+}
+
+$Sitemap->createSitemapIndex('http://mcpestats.com/');

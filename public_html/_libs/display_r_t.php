2ef88b854a43ac55fc8e84aedf7688614aadf986
Michael Yoo <michael@yoo.id.au>
2014-08-09 17:22:04 +0930

    .

--- public_html/_libs/display_r_t.php
+++ public_html/_libs/display_r_t.php
@@ -8,12 +8,23 @@
 
 function display_r_t($name, $ip, $players, $maxplayers, $onlinestatus, $port, $customname, $whitelist, $id, $serverversion)
 {
+    preg_match('/\d+(\.\d+)+/', $serverversion, $matches);
+    $serverversion = $matches[0];
+    var_dump($matches);
+
     echo "<tr>";
     echo "<td><a href=/server/$id>$name</a></td>";
 
-    echo "<td>$customname</td>";
-
     echo "<td style='white-space: nowrap;'>$serverversion</td>";
+
+    if($whitelist == 0)
+        echo '<td><span class="label label-success">Public</span></td>';
+    else if($whitelist == 1)//Whitelist
+        echo '<td><span class="label label-important">Whitelisted</span></td>';
+    else if($whitelist == 2)//Registdation
+        echo '<td><span class="label label-warning">Registdation</span></td>';
+    else
+        echo '<td><span class="label label-important">Unknown</span></td>';
 
     echo "
           <td>$ip:$port</td>

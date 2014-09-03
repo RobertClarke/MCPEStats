6acc24b0702ec2b4142065f92cf253e7c21c97a8
RobertClarke <robert@rjfc.net>
2014-09-02 21:25:43 -0700

    Create links.php

---
+++ public_html/links.php
@@ -0,0 +1,35 @@
+<?php
+require_once(__DIR__.'/_layout/header.php');
+?>
+
+    <div id="legend">
+      <legend class="">Links</legend>
+    </div>
+    
+<center>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- MCPEStats Header 2 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-3736311321196703"
+     data-ad-slot="4773859071"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
+
+<div class="eight columns">
+  <fieldset>
+    <p><strong><a href="http://mcpehub.com/seeds">Minecraft PE Seeds</a></strong></br>
+    Helping you find the best Minecraft PE seeds.
+    <p><strong><a href="http://mcpehub.com/maps">Minecraft PE Maps</a></strong></br>
+    Helping you find the best Minecraft PE seeds.
+  </fieldset>
+</div>
+</div>
+</div>
+
+<?php
+
+	include '_layout/footer.php';
+?>

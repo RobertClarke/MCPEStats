43e4ef7283f2972c30aded45998b15e34678b20d
RobertClarke <robert@rjfc.net>
2014-10-22 00:22:18 -0700

    Update constants.php

--- public_html/_libs/constants.php
+++ public_html/_libs/constants.php
@@ -9,8 +9,8 @@
 
 define('DB_NAME', "mcpestat_MCPE");
 define('DB_HOST', "localhost");
-define('DB_PASS', "Lx0J42B6G788btP");
-define('DB_USER', "root");
+define('DB_PASS', "q^6e?A;F?C@+");
+define('DB_USER', "mcpestat_MCPE");
 
 define('BLANK_DESCRIPTION_MESSAGE', "The Owner has not added any server descriptions yet.");
 define('BLANK_SERVER_RULES_MESSAGE', "The Owner has not added any server rules yet.");

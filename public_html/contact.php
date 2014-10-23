9453199169bd08d362aaf4d3c37795737a596749
RobertClarke <robert@rjfc.net>
2014-10-22 22:29:42 -0700

    Create contact.php

---
+++ public_html/contact.php
@@ -0,0 +1,42 @@
+<?php
+require_once(__DIR__.'/_layout/header.php');
+?>
+
+    <div id="legend">
+      <legend class="">Links</legend>
+    </div>
+    
+<center>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- MCPEStats Header 2 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-3736311321196703"
+     data-ad-slot="4773859071"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</center>
+
+<div class="eight columns">
+  <fieldset>
+    <!-- Do not change the code! -->
+    <a id="foxyform_embed_link_603889" href="http://www.foxyform.com/">foxyform</a>
+    <script type="text/javascript">
+    (function(d, t){
+       var g = d.createElement(t),
+           s = d.getElementsByTagName(t)[0];
+       g.src = "http://www.foxyform.com/js.php?id=603889&sec_hash=a05bd0e575b&width=350px";
+       s.parentNode.insertBefore(g, s);
+    }(document, "script"));
+    </script>
+    <!-- Do not change the code! -->
+  </fieldset>
+</div>
+</div>
+</div>
+
+<?php
+
+	include '_layout/footer.php';
+?>

96c8d4bd7cf5af313c93c6321a192c5679254a63
Michael Yoo <michael@yoo.id.au>
2015-02-13 21:17:19 +1030

    Add priority servers

--- public_html/_libs/display_r_t.php
+++ public_html/_libs/display_r_t.php
@@ -39,3 +39,37 @@ function display_r_t($name, $ip, $players, $maxplayers, $onlinestatus, $port, $c
 
     return true;
 }
+
+function display_priority_r_t($name, $ip, $players, $maxplayers, $onlinestatus, $port, $customname, $whitelist, $id, $serverversion)
+{
+	preg_match('/\d+(\.\d+)+/', $serverversion, $matches);
+	$serverversion = $matches[0];
+
+	echo "<tr>";
+	echo "<td><a href=/server/$id><strong>$name</strong></a></td>";
+
+	echo "<td style='white-space: nowrap;'><strong>$serverversion</strong></td>";
+
+	if($whitelist == 0)
+		echo '<td><span class="label label-success">Public</span></td>';
+	else if($whitelist == 1)//Whitelist
+		echo '<td><span class="label label-important">Whitelisted</span></td>';
+	else if($whitelist == 2)//Registdation
+		echo '<td><span class="label label-warning">Registdation</span></td>';
+	else
+		echo '<td><span class="label label-important">Unknown</span></td>';
+
+	echo "
+          <td><strong>$ip:$port</strong></td>
+          <td><strong>$players/$maxplayers</strong></td><td>";
+	if($onlinestatus == "Online")
+		echo '<span class="label label-success">';
+	else if($onlinestatus == "Offline")
+		echo '<span class="label label-important">';
+	else
+		echo '<span class="label label-warning">';
+	echo "$onlinestatus</td>
+      </tr>";
+
+	return true;
+}
